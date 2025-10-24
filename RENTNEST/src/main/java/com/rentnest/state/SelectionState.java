c30e0c5761ea0e928b3bb1ab846b08ed1e807904
richie404 <136493208+richie404@users.noreply.github.com>
2025-10-24 21:26:22 +0600

    Delete RENTNEST directory

--- RENTNEST/src/main/java/com/rentnest/state/SelectionState.java
+++
@@ -1,8 +0,0 @@
-package com.rentnest.state;
-
-public class SelectionState {
-    private static Integer listingId;
-    public static void set(Integer id) { listingId = id; }
-    public static Integer get() { return listingId; }
-    public static void clear() { listingId = null; }
-}
